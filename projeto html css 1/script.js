ece1865439b1fd2e915281ccbabb95e96d8b210c
victorfsfirmino <122645561+victorfsfirmino@users.noreply.github.com>
2023-03-25 20:38:19 +0000

    inicio apenas menu basico

---
+++ projeto html css 1/script.js
@@ -0,0 +1,10 @@
+
+let menuExtra = document.querySelector('.menuExtra')
+
+function botaoOpen() {
+    menuExtra.classList.add('open')
+}
+
+function botaoClose() {
+    menuExtra.classList.remove('open')
+}
